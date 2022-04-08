1260f10a782e1c8c82bbf7503f7de07a02ffbfa4
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-04-08 14:16:34 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
