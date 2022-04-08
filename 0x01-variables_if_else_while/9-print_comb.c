43f2c947b428307650fafc927820d90de1b05a3a
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-04-08 16:03:16 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints a serie of numbers with commas
  *
- * Return: Always success
+ * Return: Always (Success);
  */
 int main(void)
 {
-int numcomb;
-for (numcomb = '10'; numcomb <= '99'; numcomb++)
+int c;
+for (c = '0'; c <= '9'; c++)
 {
-putchar(numcomb);
+putchar(c);
+if (c != '9')
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
