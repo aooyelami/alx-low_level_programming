c4915d8be42c09c8dadd6064770a64759230132b
aooyelami <49617701+aooyelami@users.noreply.github.com>
2022-04-21 23:33:42 +0100

    Updated keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,47 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int main()
-{
-    int i = 0;
-    int n = 0;
-    int randomizer = 0;
-    srand((unsigned int)(time(NULL)));
-    char numbers [] = "1234567890";
-    char letter [] = "abcdefghijklmnoqprstuvwyzx";
-    char letterr [] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-    char symbols [] = "!@#$%^&*(){}[]:<>?,./";
-    printf("\nHow long password:");
-    scanf("%d", &n);
-    char password[n];
-    randomizer = rand() % 4;
-    for (i=0;i<n;i++)
-    {
-        if(randomizer == 1)
-        {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2)
-        {
-            password[i] = symbols[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3)
-        {
-            password[i] = letterr[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else
-        {
-            password[i] = letter[rand() % 21];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-    return main();
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+  
+ /** 
+  * main - program that generates random valid 
+  * passwords for the program 101-crackme 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+  
+int main(void) 
+{ 
+         int i, sum, n; 
+         int pass[100]; 
+  
+         sum = 0; 
+  
+         srand(time(NULL)); 
+  
+         for (i = 0; i < 100; i++) 
+         { 
+                 pass[i] = rand() % 78; 
+                 sum += (pass[i] + '0'); 
+                 putchar(pass[i] + '0'); 
+                 if ((2772 - sum) - '0' < 78) 
+                 { 
+                         n = 2772 - sum - '0'; 
+                         sum += n; 
+                         putchar(n + '0'); 
+                         break; 
+                 } 
+         } 
+  
+         return (0); 
 }
