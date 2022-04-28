d93932e7722dd4b5a5d037f304652a24f9cf0bab
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-04-28 21:26:01 +0100

    Added main header file and task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string recursively
+ * @s: char
+ * Return: voide
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
