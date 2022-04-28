c1fe416c89b0970098ab6518dfd5bac159a4a2c0
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-04-28 21:49:29 +0100

    Added task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: char
+ * Return: int - length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+
+/**
+ * aux - aux function where are all the logic
+ * @s: char
+ * @rev: int dinamic
+ * @p: int fixed
+ * Return: int - length
+ */
+int aux(char *s, int rev, int p)
+{
+	rev--;
+	if (*s == s[rev])
+	{
+		if (rev < (p - 1) / 2)
+			return (1);
+		return (1 * aux(++s, --rev, p));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - check palindrome word
+ * @s: char
+ * Return: int i if yes, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int p;
+
+	p = _strlen_recursion(s);
+	if (p == 0)
+		return (1);
+	if (*s && s)
+		return (aux(s, p, p));
+	return (0);
+}
