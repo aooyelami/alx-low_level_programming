a430f4d1f6c5b43978e177a18abb5fc702a63618
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-05 10:20:12 +0100

    Added task0, main header, and README files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and init with c
+ * @size: size of array
+ * @c: char
+ * Return: char pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * size);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+	return (p);
+}
