5ee020782666722f20e23dcae6fa944c95f04e19
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-06 10:00:51 +0100

    Added tasks

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case
+ * @c: char
+ * Return: int
+ */
+int _isupper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
