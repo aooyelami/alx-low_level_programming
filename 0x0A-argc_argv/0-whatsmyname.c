d4bac666c94c7d46e5bb1a8848381c11687d0bed
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-06 10:51:54 +0100

    Added task0, header file and README file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
