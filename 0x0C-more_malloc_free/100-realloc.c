4f533e519c5bdddf6425c03f8f5b637de7fa6a8e
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-09 21:06:01 +0100

    Added task100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates memory
+ * @ptr: prev pointer
+ * @old_size: prev size
+ * @new_size: new size
+ * Return: void pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+		ptr = malloc(new_size);
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
