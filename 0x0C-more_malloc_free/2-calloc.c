238c315667aa7aabded87ab4b78f9a9f4b56d818
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-09 20:59:13 +0100

    Added task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: # elements
+ * @size: size of elements
+ * Return: void, just allocate memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+	unsigned int index = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+	for (; index < (nmemb * size); index++)
+		*(mem + index) = 0;
+	return (mem);
+}
