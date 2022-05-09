d654d5a9c15160eb56fb66216302d6fe6e8f5d48
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-09 20:57:52 +0100

    Added task1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string1
+ * @s2: string 2
+ * @n: limit
+ * Return: newConcat string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newConcat = NULL;
+	unsigned int size1 = 0, size2 = 0, index = 0, limit = 0, size = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	for (; s1[size1]; size1++)
+	{};
+	for (; s2[size2]; size2++)
+	{};
+	if (n >= size2)
+		n = size2;
+	size = size1 + n + 1;
+	newConcat = malloc(size);
+	if (!newConcat)
+		return (NULL);
+	for (; s1[index]; index++)
+		newConcat[index] = s1[index];
+	for (; limit < n; index++, limit++)
+		newConcat[index] = s2[limit];
+	newConcat[index] = '\0';
+	return (newConcat);
+}
