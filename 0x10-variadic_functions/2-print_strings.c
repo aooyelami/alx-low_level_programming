300dc3009977232b538db4bb6b62f5037a0dbcbd
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-16 21:50:08 +0100

    Corrected print string for betty style

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
-		aux = va_arg(arguments, char*);
+		ntg = va_arg(arguments, char*);
 		if (ntg == NULL)
 			printf("(nil)");
 		else
