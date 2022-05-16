6e39815a5c09d46bdf887f572cd47db3a8c606b1
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-16 21:47:16 +0100

    Corrected print number and print string tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,8 @@
 
 /**
  * print_numbers - A function that prints numbers
- * 
+ * @separator is the string to be printed between numbers
+ * @n is the number of integers passed to the function
  * Return: Sum of all parameters
  */
 
@@ -12,17 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list arguments;
 	unsigned int i;
-	int aux = 0;
+	int alt = 0;
 
 
 	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
-		aux = va_arg(arguments, int);
+		alt = va_arg(arguments, int);
 		if (i < (n - 1) && separator != 0)
-			printf("%d%s", aux, separator);
+			printf("%d%s", alt, separator);
 		else
-			printf("%d", aux);
+			printf("%d", alt);
 	}
 	va_end(arguments);
 	printf("\n");
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,6 +3,8 @@
 
 /**
  * print_strings - function that prints strings
+ * @separator is the string to be printed between the strings
+ * @n  is the number of strings passed to the function
  * Returns nothing
  */
 
@@ -10,20 +12,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arguments;
 	unsigned int i;
-	char *aux;
+	char *ntg;
 
 	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
 		aux = va_arg(arguments, char*);
-		if (aux == NULL)
+		if (ntg == NULL)
 			printf("(nil)");
 		else
 		{
 			if (i < (n - 1) && separator != 0)
-				printf("%s%s", aux, separator);
+				printf("%s%s", ntg, separator);
 			else
-				printf("%s", aux);
+				printf("%s", ntg);
 		}
 	}
 	va_end(arguments);
