dcf9291bc97a55ad32481a1d9ccc9092e97c8d17
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-25 00:31:19 +0100

    Added task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - Prototype that returns num of element
+ * @h: linked list
+ * Return: num of elemets
+ */
+size_t list_len(const list_t *h)
+{
+	int strt = 0;
+
+	for (; h; el++)
+		h = h->next;
+
+	return (strt);
+}
