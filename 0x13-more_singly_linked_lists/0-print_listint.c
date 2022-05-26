87658da1bb0c5562f815bfb92899ebd07acd1ac9
Adeyemi Oyelami <adeoyelami@gmail.com>
2022-05-27 00:28:50 +0100

    corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t cnt = 0;
 
 	while (h != NULL)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
-		count++;
+		cnt++;
 	}
-	return (count);
+	return (cnt);
 }
